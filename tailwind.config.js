d08f118899e63e021f3c08b1aaa8fbca4ff6be1f
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-05-11 23:50:55 +0800

    improve tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
-
 module.exports = {
 	content: [
-		"./public/**/*.{html,js,ts,jsx,tsx,php}",
-		"./routes/**/*.php",
-		"./views/**/*.php"
+		"./**/*.{php,html,blade.php,js,ts,jsx,tsx}"
 	],
 	theme: {
-		extend: {},
+		extend: {
+			screens: {
+				mobile: '480px',
+				tablet: '768px',
+				desktop: '1024px',
+			},
+			fontFamily: {
+				sans: ['ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
+				serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
+				mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+				poppins: ['Poppins', 'sans-serif'],
+			}
+		},
 	},
-	purge: false,
 	plugins: [],
 }
