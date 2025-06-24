f667e4d068226ee5a9db89c69a8f6c5f0fe34eaa
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-06-24 21:04:40 +0800

    cleanup

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,6 @@ module.exports = {
 				mobile: '480px',
 				tablet: '768px',
 				desktop: '1024px',
-			},
-			fontFamily: {
-				sans: ['ui-sans-serif', 'system-ui', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"'],
-				serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
-				mono: ['ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
-				poppins: ['Poppins', 'sans-serif'],
 			}
 		},
 	},
