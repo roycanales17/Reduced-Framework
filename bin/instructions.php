077fbb3445b571566a706a63b9c11dd264606aec
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-06-25 19:27:46 +0800

    add instructions

---
+++ bin/instructions.php
@@ -0,0 +1,11 @@
+<?php
+
+echo PHP_EOL;
+echo "\033[32mProject created! Next steps:\033[0m" . PHP_EOL;
+echo "  1. composer update" . PHP_EOL;
+echo "  2. npm install" . PHP_EOL;
+echo "  3. npm run watch" . PHP_EOL;
+echo "  4. php artisan serve" . PHP_EOL;
+echo PHP_EOL;
+echo "\033[36m(Optional) Run composer setup to do all the above at once.\033[0m" . PHP_EOL;
+echo PHP_EOL;
