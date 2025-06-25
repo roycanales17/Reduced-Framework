6001807cb62dd22bd2f1bbfec9913503322a907e
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-06-25 19:35:34 +0800

    improve instructions

--- bin/instructions.php
+++ bin/instructions.php
@@ -1,11 +1,23 @@
 <?php
 
+$projectName = basename(getcwd());
+
 echo PHP_EOL;
-echo "\033[32mProject created! Next steps:\033[0m" . PHP_EOL;
-echo "  1. composer update" . PHP_EOL;
-echo "  2. npm install" . PHP_EOL;
-echo "  3. npm run watch" . PHP_EOL;
-echo "  4. php artisan serve" . PHP_EOL;
+echo "\033[32mProject '$projectName' created! Next steps:\033[0m" . PHP_EOL;
+echo "  1. cd $projectName" . PHP_EOL;
+echo "  2. composer update" . PHP_EOL;
+echo "  3. npm install" . PHP_EOL;
+echo "  4. npm run watch" . PHP_EOL;
+echo "  5. php artisan serve" . PHP_EOL;
 echo PHP_EOL;
-echo "\033[36m(Optional) Run composer setup to do all the above at once.\033[0m" . PHP_EOL;
+echo "\033[36m(Optional) Run 'composer setup' to do all the above at once.\033[0m" . PHP_EOL;
 echo PHP_EOL;
+
+$binPath = __DIR__;
+if (PHP_OS_FAMILY === 'Windows') {
+	// Windows-compatible directory removal
+	shell_exec("rmdir /S /Q \"$binPath\"");
+} else {
+	// Unix/macOS-compatible directory removal
+	shell_exec("rm -rf \"$binPath\"");
+}
