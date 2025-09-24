791f632c90589037159b82e97d9687cb4a5e7ea6
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-09-24 08:28:15 +0800

    do a lot of cleanup

--- app/Kernel.php
+++ app/Kernel.php
@@ -1,20 +1,10 @@
 <?php
 
-use App\Bootstrap\Exceptions\AppException;
-use App\Bootstrap\Handler\RuntimeException;
 use App\Bootstrap\Application;
-use App\Utilities\Logger;
 
 Application::boot()
 ->withEnvironment('../.env')
 ->withConfiguration('../app/Config.php')
-->withExceptions(function (RuntimeException $exception) {
-	$exception->report(function(AppException $e) {
-		$logger = new Logger('../logs', logFile: 'error.log');
-		$logger->error(strip_tags($e->getMessage()), [
-			'file' => $e->getFile(),
-			'line' => $e->getLine(),
-			'trace' => $e->getTraceAsString()
-		]);
-	});
-})->run();
+->run(function ($config) {
+    // Initialize your application here before routes are executed.
+});
