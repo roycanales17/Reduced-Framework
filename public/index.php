2fa5c3f0122485123c540f8a90d6a5d91b21b766
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-09-24 08:33:18 +0800

    improve and rename core app

--- public/index.php
+++ public/index.php
@@ -1,17 +1,18 @@
 <?php
 
-	# By Default Errors is hidden
-	error_reporting(0);
-	ini_set('display_errors', 0);
+    // Suppress errors from being displayed to the end user during configuration.
+    error_reporting(0);
+    ini_set('display_errors', '0');
 
-	# Class Importer
-	spl_autoload_register(function ($class) {
-		$path = '../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
-		if (file_exists($path)) {
-			require_once $path;
-		}
-	});
+    // PSR-4 style class autoloader
+    spl_autoload_register(function (string $class): void {
+        $path = '../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
 
-	# Built-In Functions
-	require_once '../vendor/autoload.php';
-	require_once '../app/Kernel.php';
+        if (file_exists($path)) {
+            require_once $path;
+        }
+    });
+
+    // Load Composer dependencies and core bootstrap
+    require_once '../vendor/autoload.php';
+    require_once '../app/Bootstrap.php';
