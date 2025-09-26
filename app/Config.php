e9d1da09ce9e6e0648e21533fe8b8e3a58098647
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-09-26 08:57:13 +0800

    composer ipdate and fix naming

--- app/Config.php
+++ app/Config.php
@@ -3,36 +3,6 @@
 return [
 /*
 |--------------------------------------------------------------------------
-| Cron Job - Scheduler
-|--------------------------------------------------------------------------
-|
-| This section defines how the application handles scheduled tasks.
-|
-| - 'scheduler' : The entrypoint for the internal scheduler system.
-|                 This file is invoked by the system cron every minute.
-|                 It checks all registered tasks and runs due ones.
-|
-| - 'cron'      : Optional file where you can define or register
-|                 custom cron-based tasks (similar to routes).
-|                 This allows you to centralize all your scheduled
-|                 jobs in one place.
-|
-|
-| 🔧 System Setup:
-| ----------------
-| To enable scheduled tasks, add the following to your system crontab:
-|
-|   * * * * * /usr/bin/php /path/to/project/scheduler >> /dev/null 2>&1
-|
-| This executes the scheduler every minute. The scheduler will then
-| trigger any Artisan commands or closures that are due.
-| You only need ONE cron entry — the application handles the rest.
-*/
-'scheduler' => '/artisan',
-'cron' => '/routes/cron.php',
-
-/*
-|--------------------------------------------------------------------------
 | Preloaded Files
 |--------------------------------------------------------------------------
 |
@@ -117,6 +87,37 @@ return [
 | as Mailgun, SendGrid, and custom SMTP servers.
 |
 */
-'mailing' => require 'Mailing.php'
+'mailing' => require 'Mailing.php',
 
+/*
+|--------------------------------------------------------------------------
+| Cron Job - Scheduler
+|--------------------------------------------------------------------------
+|
+| This section defines how the application handles scheduled tasks.
+|
+| - 'scheduler' : The entrypoint for the internal scheduler system.
+|                 This file is invoked by the system cron every minute.
+|                 It checks all registered tasks and runs due ones.
+|
+| - 'cron'      : Optional file where you can define or register
+|                 custom cron-based tasks (similar to routes).
+|                 This allows you to centralize all your scheduled
+|                 jobs in one place.
+|
+| - 'artisan'   : This performs the command executed by the scheduler.
+|
+|
+| 🔧 System Setup:
+| ----------------
+| To enable scheduled tasks, add the following to your system crontab:
+|
+|   * * * * * /usr/bin/php /path/to/project/scheduler >> /dev/null 2>&1
+|
+| This executes the scheduler every minute. The scheduler will then
+| trigger any Artisan commands or closures that are due.
+| You only need ONE cron entry — the application handles the rest.
+*/
+'execute' => '/artisan',
+'cron' => '/routes/cron.php',
 ];
