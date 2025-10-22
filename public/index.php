8994cc865b840e6db58b462c837f5a405b9c444d
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-10-22 09:50:53 +0800

    do not hide the errors by default during configuration

--- public/index.php
+++ public/index.php
@@ -1,9 +1,5 @@
 <?php
 
-    // Suppress errors from being displayed to the end user during configuration.
-    error_reporting(0);
-    ini_set('display_errors', '0');
-
     // PSR-4 style class autoloader
     spl_autoload_register(function (string $class): void {
         $path = '../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
