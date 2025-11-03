d035656c57b9777a6b86a34e249e31bb49d61978
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-11-03 20:26:48 +0800

    Revert "dynamic htaccess generator"
    This reverts commit e9db6c696a27f9bcf5f10a4e795074ce94fdf489.

--- public/index.php
+++ public/index.php
@@ -1,25 +1,14 @@
 <?php
-    $rootPath = dirname(__DIR__);
-    $htaccessFile = $rootPath . '/public/.htaccess';
-    $generatorScript = $rootPath . '/htaccess.sh';
 
-    if (!file_exists($htaccessFile)) {
-        if (file_exists($generatorScript)) {
-            chmod($generatorScript, 0755);
-            shell_exec("/bin/bash $generatorScript 2>&1");
-        }
-
-        if (!file_exists($htaccessFile)) {
-            die("❌ Failed to generate public/.htaccess. Please run htaccess.sh manually.");
-        }
-    }
-
+    // PSR-4 style class autoloader
     spl_autoload_register(function (string $class): void {
         $path = '../' . lcfirst(str_replace('\\', '/', $class)) . '.php';
+
         if (file_exists($path)) {
             require_once $path;
         }
     });
 
+    // Load Composer dependencies and core bootstrap
     require_once '../vendor/autoload.php';
     require_once '../app/Bootstrap.php';
